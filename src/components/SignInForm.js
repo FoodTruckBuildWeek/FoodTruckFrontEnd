d699d453dafce6dfce8343c768b8f7ce30e26425
DJcri <djcri123@gmail.com>
2021-03-29 13:16:43 -0700

    fleshed out sign in form

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { Form, Button } from "react-bootstrap";
 
-const SingInForm = () => {
-  return <div>Hello</div>;
+const formSchema = {
+  username: "",
+  password: "",
 };
 
-export default SingInForm;
+const SignInForm = () => {
+  const [formValues, setFormValues] = useState(formSchema);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //get token from server and save to local storage
+  };
+
+  return (
+    <>
+      <h1>User Sign In Form</h1>
+      <Form className="form" onSubmit={handleSubmit}>
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Username"
+            name="username"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter Password"
+            name="password"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignInForm);
