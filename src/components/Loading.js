e31b34de764ae61e6b636abe231338aed76baf4c
DJcri <djcri123@gmail.com>
2021-04-01 11:10:19 -0700

    created loading spinner

---
+++ src/components/Loading.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <>
+      <h1>Loading...</h1>
+      <div className="lds-spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export default Loading;
